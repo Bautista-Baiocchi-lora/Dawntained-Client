c07b5f7d5edc32e9e62f055b3dd0242aa9765dad
Ethan07Scape <ethan.ducky@gmail.com>
2017-07-06 21:09:22 -0500

    Reformatted to intellij + execution error

--- src/org/bot/server/ServerLoader.java
+++ src/org/bot/server/ServerLoader.java
@@ -1,9 +1,5 @@
 package org.bot.server;
 
-import java.awt.Component;
-import java.io.IOException;
-import java.util.jar.JarFile;
-
 import org.bot.classloader.Archive;
 import org.bot.classloader.ArchiveClassLoader;
 import org.bot.classloader.JarArchive;
@@ -11,59 +7,70 @@ import org.bot.util.FileDownloader;
 import org.bot.util.reflection.ReflectedClass;
 import org.objectweb.asm.tree.ClassNode;
 
+import java.awt.*;
+import java.io.IOException;
+import java.util.jar.JarFile;
+
 public abstract class ServerLoader<T extends Component> {
 
-	private final String JAR_URL;
-	private final String SERVER_NAME;
-	private final FileDownloader downloader;
-	private final ArchiveClassLoader loader;
-	private T gameComponent;
+    private final String JAR_URL;
+    private final String SERVER_NAME;
+    private FileDownloader downloader = null;
+    private ArchiveClassLoader loader = null;
+    private T gameComponent;
 
-	public ServerLoader(String jarURL, String serverName) throws IOException {
-		this.JAR_URL = jarURL;
-		this.SERVER_NAME = serverName;
-		System.out.println("Updating " + serverName + " jar file.");
-		this.downloader = new FileDownloader(jarURL, SERVER_NAME);
-		downloader.run();
-		System.out.println("Updated " + serverName + " jar file.");
-		final JarFile jar = new JarFile(downloader.getArchivePath() + "/" + SERVER_NAME + ".jar");
-		final Archive<ClassNode> archive = new JarArchive(jar);
-		loader = new ArchiveClassLoader(archive);
-		System.out.println("Loading " + serverName + " jar file.");
-		try {
-			gameComponent = loadProtocol();
-		} catch (IllegalArgumentException | IllegalAccessException e) {
-			e.printStackTrace();
-		}
-		System.out.println(serverName + " component loaded. Starting client.");
-	}
+    public ServerLoader(String jarURL, String serverName) throws IOException {
+        this.JAR_URL = jarURL;
+        this.SERVER_NAME = serverName;
+    }
 
-	public T getGameComponent() {
-		return gameComponent;
-	}
+    public void executeServer() {
+        try {
+            System.out.println("Updating " + SERVER_NAME + " jar file.");
+            this.downloader = new FileDownloader(JAR_URL, SERVER_NAME);
+            downloader.run();
+            System.out.println("Updated " + SERVER_NAME + " jar file.");
+            final JarFile jar = new JarFile(downloader.getArchivePath() + "/" + SERVER_NAME + ".jar");
+            final Archive<ClassNode> archive = new JarArchive(jar);
+            loader = new ArchiveClassLoader(archive);
+            System.out.println("Loading " + SERVER_NAME + " jar file.");
+            try {
+                gameComponent = loadProtocol();
+            } catch (IllegalArgumentException | IllegalAccessException e) {
+                e.printStackTrace();
+            }
+            System.out.println(SERVER_NAME + " component loaded. Starting client.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	protected ArchiveClassLoader getLoader() {
-		return loader;
-	}
+    public T getGameComponent() {
+        return gameComponent;
+    }
 
-	public String getJarURL() {
-		return JAR_URL;
-	}
+    protected ArchiveClassLoader getLoader() {
+        return loader;
+    }
 
-	public String getServerName() {
-		return SERVER_NAME;
-	}
+    public String getJarURL() {
+        return JAR_URL;
+    }
 
-	public ReflectedClass getClass(String name) {
-		if (!loader.classes().containsKey(name)) {
-			try {
-				return new ReflectedClass(loader.loadClass(name));
-			} catch (ClassNotFoundException e) {
-				e.printStackTrace();
-			}
-		}
-		return new ReflectedClass(loader.classes().get(name));
-	}
+    public String getServerName() {
+        return SERVER_NAME;
+    }
 
-	public abstract T loadProtocol() throws IllegalArgumentException, IllegalAccessException;
+    public ReflectedClass getClass(String name) {
+        if (!loader.classes().containsKey(name)) {
+            try {
+                return new ReflectedClass(loader.loadClass(name));
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        return new ReflectedClass(loader.classes().get(name));
+    }
+
+    public abstract T loadProtocol() throws IllegalArgumentException, IllegalAccessException;
 }
--- src/org/bot/ui/serverselector/ServerProviderComponent.java
+++ src/org/bot/ui/serverselector/ServerProviderComponent.java
@@ -1,93 +1,88 @@
 package org.bot.ui.serverselector;
 
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.Font;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-
-import javax.swing.BorderFactory;
-import javax.swing.JComponent;
-import javax.swing.JLabel;
-import javax.swing.border.Border;
-
 import org.bot.boot.Engine;
 import org.bot.server.ServerProvider;
 
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 public class ServerProviderComponent extends JComponent {
 
-	private final ServerSelector instance;
-	private static final long serialVersionUID = 8365360607486641452L;
-	private final Font NAME_FONT = new Font("Dialog", Font.BOLD, 18);
-	private final Font INFO_FONT = new Font("Dialog", Font.ITALIC, 13);
-	private final Font DESCRIPTION_FONT = new Font("Dialog", Font.PLAIN, 11);
-	private final Border BORDER = BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.BLACK),
-			"Description");
-	private final Border COMPONENT_BORDER = BorderFactory.createLineBorder(Color.BLACK);
-	private final ServerProvider provider;
-	private final JLabel author, serverName, type, version;
+    private static final long serialVersionUID = 8365360607486641452L;
+    private final ServerSelector instance;
+    private final Font NAME_FONT = new Font("Dialog", Font.BOLD, 18);
+    private final Font INFO_FONT = new Font("Dialog", Font.ITALIC, 13);
+    private final Font DESCRIPTION_FONT = new Font("Dialog", Font.PLAIN, 11);
+    private final Border BORDER = BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.BLACK),
+            "Description");
+    private final Border COMPONENT_BORDER = BorderFactory.createLineBorder(Color.BLACK);
+    private final ServerProvider provider;
+    private final JLabel author, serverName, type, version;
 
-	public ServerProviderComponent(ServerProvider provider, ServerSelector instance) {
-		super();
-		this.provider = provider;
-		this.instance = instance;
-		setBorder(COMPONENT_BORDER);
-		setLayout(new FlowLayout());
-		author = new JLabel("Author: " + provider.getManifest().author());
-		author.setFont(INFO_FONT);
-		serverName = new JLabel(provider.getManifest().serverName());
-		serverName.setFont(NAME_FONT);
-		type = new JLabel("Type: " + provider.getManifest().type().getSimpleName());
-		type.setFont(INFO_FONT);
-		version = new JLabel("Version: " + provider.getManifest().version());
-		version.setFont(INFO_FONT);
-		setPreferredSize(new Dimension(400, 150));
-		add(serverName);
-		add(author);
-		add(type);
-		add(version);
-		setBackground(Color.GRAY);
-		addMouseListener(new MouseListener() {
+    public ServerProviderComponent(ServerProvider provider, ServerSelector instance) {
+        super();
+        this.provider = provider;
+        this.instance = instance;
+        setBorder(COMPONENT_BORDER);
+        setLayout(new FlowLayout());
+        author = new JLabel("Author: " + provider.getManifest().author());
+        author.setFont(INFO_FONT);
+        serverName = new JLabel(provider.getManifest().serverName());
+        serverName.setFont(NAME_FONT);
+        type = new JLabel("Type: " + provider.getManifest().type().getSimpleName());
+        type.setFont(INFO_FONT);
+        version = new JLabel("Version: " + provider.getManifest().version());
+        version.setFont(INFO_FONT);
+        setPreferredSize(new Dimension(400, 150));
+        add(serverName);
+        add(author);
+        add(type);
+        add(version);
+        setBackground(Color.GRAY);
+        addMouseListener(new MouseListener() {
 
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				Engine.getInstance().setServerProvider(provider);
-				instance.dispose();
-			}
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                Engine.getInstance().setServerProvider(provider);
+                Engine.getInstance().getServerLoader().executeServer();
+                instance.dispose();
+            }
 
-			@Override
-			public void mouseEntered(MouseEvent e) {
-				// TODO Auto-generated method stub
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                // TODO Auto-generated method stub
 
-			}
+            }
 
-			@Override
-			public void mouseExited(MouseEvent e) {
-				// TODO Auto-generated method stub
+            @Override
+            public void mouseExited(MouseEvent e) {
+                // TODO Auto-generated method stub
 
-			}
+            }
 
-			@Override
-			public void mousePressed(MouseEvent e) {
-				// TODO Auto-generated method stub
+            @Override
+            public void mousePressed(MouseEvent e) {
+                // TODO Auto-generated method stub
 
-			}
+            }
 
-			@Override
-			public void mouseReleased(MouseEvent e) {
-				// TODO Auto-generated method stub
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                // TODO Auto-generated method stub
 
-			}
+            }
 
-		});
+        });
 
-	}
+    }
 
-	@Override
-	public Dimension getPreferredSize() {
-		return new Dimension(getParent().getWidth(),
-				(serverName.getHeight() + type.getHeight() + version.getHeight() + author.getHeight()));
-	}
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(getParent().getWidth(),
+                (serverName.getHeight() + type.getHeight() + version.getHeight() + author.getHeight()));
+    }
 
 }
