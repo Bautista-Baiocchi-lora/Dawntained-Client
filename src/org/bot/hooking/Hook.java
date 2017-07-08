05c68cd07adc70e6c574bd9e5dc5573a157c64bb
Ethan07Scape <ethan.ducky@gmail.com>
2017-07-07 23:12:18 -0500

    Hook Loading System

--- src/org/bot/hooking/Hook.java
+++ src/org/bot/hooking/Hook.java
@@ -1,14 +1,26 @@
 package org.bot.hooking;
 
 import org.bot.Engine;
+import org.bot.util.NetUtil;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.net.URLConnection;
 
 /**
  * Created by Ethan on 7/7/2017.
  */
 public class Hook {
-    private static Hook instance;
+
+    private String HOOK_URL = null;
     private Engine engine = Engine.getInstance();
-    public Hook() {
+
+    public Hook(String hookURL) {
+        HOOK_URL = hookURL;
         init();
     }
 
@@ -16,7 +28,32 @@ public class Hook {
         /**
          * GRAB HOOKS FROM XML?
          */
-        engine.getFieldMap().put("getGameX", new FieldHook("TestClass", "TestField", -1));
+        try {
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            URLConnection connection = NetUtil.createURLConnection(HOOK_URL);
+            Document doc = dBuilder.parse(connection.getInputStream());
+            doc.getDocumentElement().normalize();
+
+            System.out.println("Root element: " + doc.getDocumentElement().getNodeName());
+            NodeList getters = doc.getElementsByTagName("add");
+            for(int i = 0; i < getters.getLength(); i++) {
+                Node n = getters.item(i);
+
+                if (n.getNodeType() == Node.ELEMENT_NODE) {
+                    Element e = (Element) n;
+                    String getter = e.getElementsByTagName("getter").item(0).getTextContent();
+                    String clazz = e.getElementsByTagName("classname").item(0).getTextContent();
+                    String field = e.getElementsByTagName("field").item(0).getTextContent();
+                    String multiplier = e.getElementsByTagName("multiplier").item(0).getTextContent();
+                    engine.getFieldMap().put(getter, new FieldHook(clazz, field, Integer.parseInt(multiplier)));
+                    //System.out.println("Getter: "+getter + " || Class: "+clazz + " || Field: "+field + " || Multiplier: "+multiplier);
+                }
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public String getClass(String getterName, boolean isField) {
@@ -33,9 +70,4 @@ public class Hook {
     public String getDesc(String getterName) {
         return engine.getMethodMap().get(getterName).getDesc();
     }
-
-    public static Hook getInstance() {
-        return instance == null ? instance = new Hook() : instance;
-    }
-
 }
