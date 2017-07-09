ec6cc23410a905c49e552b3db9eaec3b072dd845
Ethan07Scape <ethan.ducky@gmail.com>
2017-07-09 13:21:39 -0500

    inArray()

--- src/org/bot/util/Utilities.java
+++ src/org/bot/util/Utilities.java
@@ -31,4 +31,21 @@ public class Utilities {
 		}
 		return sb.toString().trim();
 	}
+	public static boolean inArray(String string, String[] strings) {
+		if (string == null)
+			return false;
+		for (String s : strings) {
+			if (s != null && s.equalsIgnoreCase(string))
+				return true;
+		}
+		return false;
+	}
+
+	public static boolean inArray(int i, int[] array) {
+		for (int j : array) {
+			if (j == i)
+				return true;
+		}
+		return false;
+	}
 }
